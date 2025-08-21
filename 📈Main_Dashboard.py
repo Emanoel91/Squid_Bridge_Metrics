4c19089511e4137fa1478adb727de430b041834e
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-21 18:36:46 +0330

    Update 📈Main_Dashboard.py

--- 📈Main_Dashboard.py
+++ 📈Main_Dashboard.py
@@ -40,13 +40,33 @@ st.info("📊Charts initially display data for a default time range. Select a cu
 st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Snowflake Connection ----------------------------------------------------------------------------------------
+snowflake_secrets = st.secrets["snowflake"]
+user = snowflake_secrets["user"]
+account = snowflake_secrets["account"]
+private_key_str = snowflake_secrets["private_key"]
+warehouse = snowflake_secrets.get("warehouse", "")
+database = snowflake_secrets.get("database", "")
+schema = snowflake_secrets.get("schema", "")
+
+private_key_pem = f"-----BEGIN PRIVATE KEY-----\n{private_key_str}\n-----END PRIVATE KEY-----".encode("utf-8")
+private_key = serialization.load_pem_private_key(
+    private_key_pem,
+    password=None,
+    backend=default_backend()
+)
+private_key_bytes = private_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
 conn = snowflake.connector.connect(
-    user=st.secrets["snowflake"]["user"],
-    password=st.secrets["snowflake"]["password"],
-    account=st.secrets["snowflake"]["account"],
-    warehouse="SNOWFLAKE_LEARNING_WH",
-    database="AXELAR",
-    schema="PUBLIC"
+    user=user,
+    account=account,
+    private_key=private_key_bytes,
+    warehouse=warehouse,
+    database=database,
+    schema=schema
 )
 
 # --- Date Inputs ---------------------------------------------------------------------------------------------------
